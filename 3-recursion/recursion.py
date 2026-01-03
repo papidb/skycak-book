02910a2539e6a11026280562da40c8b93bb10f0a
Daniel Benjamin <benjamindaniel706@gmail.com>
2026-01-03 09:04:21 +0100

    chore: implement collatz sequence

--- 3-recursion/recursion.py
+++ 3-recursion/recursion.py
@@ -17,3 +17,27 @@ def get_nth_term_sequence_1(n: int) -> int:
     if n == 1:
         return 5
     return (get_nth_term_sequence_1(n - 1) * 3) - 4
+
+
+def generate_collatz_sequence(n: int) -> list:
+    """
+    Generate an array containing the first n terms of the Collatz sequence.
+    Starting with 25, if previous is even: divide by 2, if odd: multiply by 3 and add 1.
+    """
+    result = []
+    for i in range(n):
+        result.append(get_nth_term_collatz(i + 1))
+    return result
+
+
+def get_nth_term_collatz(n: int) -> int:
+    """
+    Recursively generate the nth term of the Collatz sequence.
+    Starting with 25, if previous is even: divide by 2, if odd: multiply by 3 and add 1.
+    """
+    if n == 1:
+        return 25
+    previous = get_nth_term_collatz(n - 1)
+    if previous % 2 == 0:
+        return previous / 2
+    return (previous * 3) + 1
--- 3-recursion/recursion.test.py
+++ 3-recursion/recursion.test.py
@@ -2,7 +2,7 @@ import sys
 sys.path.append('..')
 from test_utils import run_tests
 
-from recursion import generate_sequence_1, get_nth_term_sequence_1
+from recursion import generate_sequence_1, get_nth_term_sequence_1, generate_collatz_sequence, get_nth_term_collatz
 
 tests = [
     # generate_sequence_1 tests
@@ -92,6 +92,115 @@ tests = [
         'function': get_nth_term_sequence_1,
         'input': 10,
         'output': 59051
+    },
+
+    # generate_collatz_sequence tests
+    {
+        'function': generate_collatz_sequence,
+        'input': 1,
+        'output': [25]
+    },
+    {
+        'function': generate_collatz_sequence,
+        'input': 2,
+        'output': [25, 76]
+    },
+    {
+        'function': generate_collatz_sequence,
+        'input': 3,
+        'output': [25, 76, 38]
+    },
+    {
+        'function': generate_collatz_sequence,
+        'input': 4,
+        'output': [25, 76, 38, 19]
+    },
+    {
+        'function': generate_collatz_sequence,
+        'input': 5,
+        'output': [25, 76, 38, 19, 58]
+    },
+    {
+        'function': generate_collatz_sequence,
+        'input': 6,
+        'output': [25, 76, 38, 19, 58, 29]
+    },
+    {
+        'function': generate_collatz_sequence,
+        'input': 7,
+        'output': [25, 76, 38, 19, 58, 29, 88]
+    },
+    {
+        'function': generate_collatz_sequence,
+        'input': 10,
+        'output': [25, 76, 38, 19, 58, 29, 88, 44, 22, 11]
+    },
+    {
+        'function': generate_collatz_sequence,
+        'input': 0,
+        'output': []
+    },
+
+    # get_nth_term_collatz tests
+    {
+        'function': get_nth_term_collatz,
+        'input': 1,
+        'output': 25
+    },
+    {
+        'function': get_nth_term_collatz,
+        'input': 2,
+        'output': 76
+    },
+    {
+        'function': get_nth_term_collatz,
+        'input': 3,
+        'output': 38
+    },
+    {
+        'function': get_nth_term_collatz,
+        'input': 4,
+        'output': 19
+    },
+    {
+        'function': get_nth_term_collatz,
+        'input': 5,
+        'output': 58
+    },
+    {
+        'function': get_nth_term_collatz,
+        'input': 6,
+        'output': 29
+    },
+    {
+        'function': get_nth_term_collatz,
+        'input': 7,
+        'output': 88
+    },
+    {
+        'function': get_nth_term_collatz,
+        'input': 8,
+        'output': 44
+    },
+    {
+        'function': get_nth_term_collatz,
+        'input': 9,
+        'output': 22
+    },
+    {
+        'function': get_nth_term_collatz,
+        'input': 10,
+        'output': 11
+    },
+    {
+        'function': get_nth_term_collatz,
+        'input': 11,
+        'output': 34
+    },
+    {
+        'function': get_nth_term_collatz,
+        'input': 12,
+        'output': 17
     },
 ]
 
